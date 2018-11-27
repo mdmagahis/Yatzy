69f14b761d2169ddadb61bda9fd1354d57c6bcda
Mark Magahis <mark.magahis@gmail.com>
2018-11-27 14:15:00 -0600

    add module

--- yatzy/src/yatzy.erl
+++ yatzy/src/yatzy.erl
@@ -1,4 +1,6 @@
--type slot() :: 'ones' | 'twos' | 'threes' | 'fours' | 'fives' | 'sixes' | 'one pair' | 'two pairs' | 'three of a kind' |  'four of a kind' |  'small straight' |  'large straight' |  'full house' |  'chance' | 'yatzy' | 
+-module(yatzy).
+
+-type slot() :: 'ones' | 'twos' | 'threes' | 'fours' | 'fives' | 'sixes' | 'one pair' | 'two pairs' | 'three of a kind' |  'four of a kind' |  'small straight' |  'large straight' |  'full house' |  'chance' | 'yatzy'.
 
 -type slot_type() :: 'upper' | 'lower'.
 
