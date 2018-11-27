01ec12ed903d37dbb73b30ec2b8a27057be7fc1f
Mark Magahis <mark.magahis@gmail.com>
2018-11-27 13:24:23 -0600

    add lower section scoring categories

--- yatzy/src/yatzy.erl
+++ yatzy/src/yatzy.erl
@@ -1,4 +1,4 @@
--type slot() :: 'ones' | 'twos' | 'threes' | 'fours' | 'fives' | 'sixes' |
+-type slot() :: 'ones' | 'twos' | 'threes' | 'fours' | 'fives' | 'sixes' | 'one pair' | 'two pairs' | 'three of a kind' |  'four of a kind' |  'small straight' |  'large straight' |  'full house' |  'chance' | 'yatzy' | 
 
 -type slot_type() :: 'upper' | 'lower'.
 
