78b5bd536ea649a6cdec415c9670dfa0e38e9d94
Mark Magahis <mark.magahis@gmail.com>
2018-11-27 15:21:50 -0600

    add functions for yatzy, three_of_a_kind, and four_of_a_kind

--- yatzy/src/yatzy_score.erl
+++ yatzy/src/yatzy_score.erl
@@ -3,4 +3,28 @@
 -spec calc(yatzy:slot(), yatzy:roll()) -> non_neg_integer().
 
 calc(chance,Roll) ->
-  lists:sum(Roll).
+  lists:sum(Roll);
+
+calc(three_of_a_kind,Roll) ->
+  three_of_a_kind_test(lists:sort(Roll));
+
+calc(four_of_a_kind,Roll) ->
+  four_of_a_kind_test(lists:sort(Roll));
+
+calc(yatzy,Roll) ->
+  yatzy_score(Roll).
+
+three_of_a_kind_test([_,_,X,X,X]) ->
+  X*3;
+three_of_a_kind_test([_,X,X,X,_]) ->
+  X*3;
+three_of_a_kind_test([X,X,X,_,_]) ->
+  X*3.
+
+four_of_a_kind_test([_,X,X,X,X]) ->
+  X*4;
+four_of_a_kind_test([X,X,X,X,_]) ->
+  X*4.
+
+yatzy_score([X,X,X,X,X]) ->
+  50.
