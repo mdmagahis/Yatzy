c1baa4c94e558b5d7072b8fa0ba89b781a7f2623
Mark Magahis <mark.magahis@gmail.com>
2018-11-28 13:18:55 -0600

    combine export statements and fix get

--- yatzy/src/yatzy_sheet.erl
+++ yatzy/src/yatzy_sheet.erl
@@ -1,29 +1,24 @@
 -module(yatzy_sheet).
 -type t() :: map().
 
--export([new/0]).
--spec new() -> t().
+-export([new/0,
+         fill/3,
+         get/2,
+         upper_total/1,
+         bonus/1,
+         lower_total/1,
+         total/1]).
 
--export([fill/3]).
+-spec new() -> t().
 -spec fill(yatzy:slot(), yatzy:roll(), t()) -> {'ok', t()}
                                              | 'already_filled'
                                              | 'invalid_slot'.
-
--export([get/2]).
 -spec get(yatzy:slot(), t()) -> {'filled', non_neg_integer()}
                               | 'invalid_slot'
                               | 'empty'.
-
--export([upper_total/1]).
 -spec upper_total(t()) -> non_neg_integer().
-
--export([bonus/1]).
 -spec bonus(to()) -> 0 | 50.
-
--export([lower_total/1]).
 -spec lower_total(t()) -> non_neg_integer().
-
--export([total/1]).
 -spec total(t()) -> non_neg_integer().
 
 % definitions--
@@ -32,31 +27,17 @@ new() ->
 
 % fill(yatzy,Roll,Map) ->
 
-get(_,#{}) ->
-  'empty'.
-get(Slot,_) ->
+get(Slot,Sheet) ->
   case lists:member(Slot,yatzy:slot()) of
     true ->
-      % ok;
-      case Slot of
-        'ones'            -> {'filled', 0}; % MDM
-        'twos'            -> ok;
-        'threes'          -> ok;
-        'fours'           -> ok;
-        'fives'           -> ok;
-        'sixes'           -> ok;
-        'one_pair'        -> ok;
-        'two_pairs'       -> ok;
-        'three_of_a_kind' -> ok;
-        'four_of_a_kind'  -> ok;
-        'small_straight'  -> ok;
-        'large_straight'  -> ok;
-        'full_house'      -> ok;
-        'chance'          -> ok;
-        'yatzy'           -> ok
+      case maps:get(Slot, Sheet, empty) of
+        empty ->
+          empty;
+        Value ->
+          {filled, Value}
       end;
     false ->
-      'invalid_slot'
+      invalid_slot
   end.
 
 total(map) ->
