1fdff8df23ff851c31241c653fb84e6a4328d517
Mark Magahis <mark.magahis@gmail.com>
2018-12-05 11:04:21 -0600

    supervisor using static child

--- yatzy/src/yatzy_sup.erl
+++ yatzy/src/yatzy_sup.erl
@@ -8,8 +8,8 @@
 -behaviour(supervisor).
 
 %% API
-% -export([start_link/0]).
--export([start_link/2]).
+-export([start_link/0]).
+% -export([start_link/2]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -20,10 +20,10 @@
 %% API functions
 %%====================================================================
 
-% start_link() ->
-    % supervisor:start_link({local, ?SERVER}, ?MODULE, []).
-start_link(john, ChildTurnName) ->
-  supervisor:start_link({local, ?SERVER}, ?MODULE, [john, ChildTurnName]).
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+% start_link(john, ChildTurnName) ->
+  % supervisor:start_link({local, ?SERVER}, ?MODULE, [john, ChildTurnName]).
 
 %%====================================================================
 %% Supervisor callbacks
@@ -33,11 +33,11 @@ start_link(john, ChildTurnName) ->
 %% Optional keys are restart, shutdown, type, modules.
 %% Before OTP 18 tuples must be used to specify a child. e.g.
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
-init([john, ChildTurnName]) ->
+init([]) ->
   % {ok, {{one_for_all, 0, 1}, []}}.
   ChildPlayer = {john, {otp_yatzy_player, start_link, [john]}, permanent, 2000, worker, [otp_yatzy_player]},
-  ChildTurn = {ChildTurnName, {otp_yatzy_turn, start_link, []}, permanent, 2000, worker, [otp_yatzy_turn]},
-  {ok, {{one_for_one, 0, 1}, [ChildPlayer, ChildTurn]}}.
+  % ChildTurn = {ChildTurnName, {otp_yatzy_turn, start_link, []}, permanent, 2000, worker, [otp_yatzy_turn]},
+  {ok, {{one_for_one, 0, 1}, [ChildPlayer]}}.
 
 %%====================================================================
 %% Internal functions
